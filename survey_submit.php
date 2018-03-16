047ef37318f3813a8eaabdc662fa7c0ac54e9889
etrewitt <etrewitt@users.noreply.github.com>
2018-03-16 05:56:57 -0700

    add feedback printouts comparing to averages

--- survey_submit.php
+++ survey_submit.php
@@ -63,7 +63,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 	$water_used += $shower_used + $brush_used + $flush_used + $laundry_used + $dishes_used;
 
-
 	printf("<p>You use %.2f gallons of water per month:</p>\n", $water_used);
 	print("<ul>\n");
 	printf("\t<li>%.2f%% from showering</li>\n",      100 * $shower_used / $water_used);
@@ -72,28 +71,36 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	printf("\t<li>%.2f%% from laundry</li>\n",        100 * $laundry_used / $water_used);
 	printf("\t<li>%.2f%% from washing dishes</li>\n", 100 * $dishes_used / $water_used);
 	print("</ul>\n");
+
+	if ($shower_used > 400) {
+		print("<p>You use significantly more water when showering than the average Californian. Next time you shower, try this trick to conserve water: first, use the first 30-60 seconds to get yourself wet. Next, turn off the shower while you lather up with soap and put shampoo in your hair. Finally, turn on the shower for a minute or two to rinse all the soap off of your body and out of your hair.</p>");
+	}
+	if ($brush_used > 40) {
+		print("<p>You use more than a gallon of water to brush your teeth every day, which is above the Californian and the national average. To use less water while brushing your teeth, try turning on the faucet briefly to wet your toothbrush, turning it off while actually brushing your teeth, and then turning it on again to clean your toothbrush.</p>");
+	}
 	exit;
 }
 
 function calcShower($shower, $time, $freq, $shavelegs, $shaveface) {
-	$shower_used = 0; // shower + shave imo
+	$shower_used = 0; // for both shower & shave
 	if ($shower == "onCampus") {
 		$shower_used += 1.5 * $time * $freq * 4;
 	} else {
 		$shower_used += 2 * $time * $freq * 4;
 	}
+	// Here, I'm assuming that people shave with the same frequency as they shower
 	if ($shavelegs != "No") {
-		$shower_used += 1 * $freq * 4; // Here, I'm assuming that people shave with the same frequency as they shower
+		$shower_used += 1 * $freq * 4;
 	}
 	if ($shaveface != "No") {
-		$shower_used += 1 * $freq * 4; // Here, I'm assuming that people shave with the same frequency as they shower
+		$shower_used += 1 * $freq * 4;
 	}
 	return $shower_used;
 }
 
 function calcBrush($faucet, $brush_used) {
 	$brush_used = 0;
-	if ($faucet == "faucetOn") {
+	if ($faucet == "on") {
 		$brush_used += 4 * 2 * 7 * 4; // 4 gallons * 2x per day * 7 days per week * 4 weeks
 	} else {
 		$brush_used += 0.5 * 2 * 7 * 4;
@@ -110,106 +117,6 @@ function dishesCalc($dishwasher, $dishes_used, $handwash) {
 	}
 	return $dishes_used;
 }
-
-// function validateInput($id, $dept, $courseNo, $year, $quarter) {
-// 	$conn = oci_connect( 'etrewitt', '/* password here */', '//dbserver.engr.scu.edu/db11g' );
-// 	if (!$conn) {
-// 		print "<br> connection failed:";
-// 		exit;
-// 	}
-//
-// 	// make sure the course exists
-// 	$query = oci_parse(
-// 		$conn,
-// 		"SELECT *
-// 		 FROM   CourseList
-// 		 WHERE  dept = '$dept' and courseNo = '$courseNo'"
-// 	);
-// 	oci_execute($query);
-// 	$i = 0;
-// 	while (($row = oci_fetch_array($query, OCI_BOTH)) != false) {
-// 		$i++;
-// 	}
-// 	if ($i == 0) {
-// 		print "<br><b>$dept $courseNo</b> doesn't exist; valid courses in $dept are:";
-// 		$query = oci_parse(
-// 			$conn,
-// 			"SELECT (dept || ' ' || courseNo) as course, name, units
-// 			 FROM   CourseList
-// 			 WHERE  dept = '$dept'"
-// 		);
-// 		oci_execute($query);
-//
-// 		// print valid courses in the selected department
-// 		echo "\n<table>\n";
-// 		echo "\t<tr><th>Course No.</th><th>Course title</th><th>Units</th></tr>\n\t";
-// 		while (($row = oci_fetch_array($query, OCI_BOTH)) != false) {
-// 			echo "<tr>\n";
-// 			echo "\t\t<td>" . $row[0] . "</td>\n";
-// 			echo "\t\t<td>" . $row[1] . "</td>\n";
-// 			echo "\t\t<td>" . $row[2] . "</td>\n";
-// 			echo "\t</tr>";
-// 		}
-// 		echo "\n</table>\n";
-// 		echo '<br><a href="enter_course.html" class="button" style="font-size: 10pt;">Add a different course</a><br>';
-// 		OCILogoff($conn);
-// 		return false;
-// 	}
-//
-// 	// make sure the student isn't already taking this course
-// 	$query = oci_parse(
-// 		$conn,
-// 		"SELECT *
-// 		 FROM   CourseRequests
-// 		 WHERE  studentID = '$id' and
-// 		        dept      = '$dept' and
-// 						courseNo  = '$courseNo'"
-// 	);
-// 	oci_execute($query);
-// 	$i = 0;
-// 	while (($row = oci_fetch_array($query, OCI_BOTH)) != false) {
-// 		$i++;
-// 	}
-// 	if ($i != 0) {
-// 		print "<p>$id is already registered for $dept $courseNo</p>";
-// 		echo '<a href="enter_course.html" class="button" style="font-size: 10pt;">Add a different course</a><br>';
-// 		OCILogoff($conn);
-// 		return false;
-// 	}
-//
-// 	OCILogoff($conn);
-// 	return true;
-// }
-//
-// function addCourse($id, $dept, $courseNo, $year, $quarter) {
-// 	// connect to your database. Type in your username, password and the DB path
-// 	$conn = oci_connect( 'etrewitt', '/* password here */', '//dbserver.engr.scu.edu/db11g' );
-// 	if (!$conn) {
-// 		print "<br> connection failed:";
-// 		exit;
-// 	}
-//
-// 	$query = oci_parse(
-// 		$conn,
-// 		"INSERT INTO CourseRequests values ('$id', '$dept', '$courseNo', '$quarter', $year)"
-// 	);
-// 	if (! @oci_execute($query)) {
-// 		$err = oci_error($query);
-// 		$code = $err["code"];
-// 		if ($code == 20000) {
-// 			echo "<p><b>Error $code:</b> Failed to meet the prerequisite(s) for $dept $courseNo.</p>\n";
-// 			echo '<a href="enter_course.html" class="button" style="font-size: 10pt;">Add a different course</a><br>';
-// 		} elseif ($code == 20001) {
-// 			echo "<p><b>Error $code:</b> Already completed $dept $courseNo.</p>\n";
-// 			echo '<a href="enter_course.html" class="button" style="font-size: 10pt;">Add a different course</a><br>';
-// 		}
-// 	} else {
-// 		$nextYear = $year + 1;
-// 		print "<p>Successfully requested $dept $courseNo for $id in the $quarter $year-$nextYear quarter.</p>";
-// 	}
-//
-// 	OCILogoff($conn);
-// }
 
 ?>
 		</main>
